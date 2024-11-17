d5ba7fe8abe6ed2ba10ad901cbb82d5b50a527ee
chungFTF <stephaniechung0729@gmail.com>
2024-11-17 11:18:08 +0800

    add DTL

--- food/views.py
+++ food/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 def index(request):
-    return HttpResponse("Hello")
+    item_list = Item.objects.all()
+
+    context = {
+        "item_list": item_list
+    }
+
+    return render(request, 'food/base.html', context)
 
 def item(request):
     return HttpResponse("This is an item view.")
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    
+    context = {
+        "item": item
+    }
+    return render(request, 'food/detail.html', context)
