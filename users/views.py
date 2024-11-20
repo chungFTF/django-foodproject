b0688cf19608497a7b16dc1b0a50f1ec7c8fedf4
chungFTF <stephaniechung0729@gmail.com>
2024-11-20 10:45:55 +0800

    add extra field in form

--- users/views.py
+++ users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 def register(request):
     # Check POST or GET
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, account OK!!')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'users/register.html', {"form": form})
 
